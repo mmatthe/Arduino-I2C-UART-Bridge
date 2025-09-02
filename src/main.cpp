6d81f811bc5c8d5a6d17175ccccf61c0dccc7da5
Maximilian Matthe <maxi.matthe@googlemail.com>
2025-09-02 07:35:57 +0200

    implement structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+int8_t address;
+bool showDebug = true;
 
 void setup() {
     Serial.begin(9600);
@@ -9,38 +11,46 @@ void setup() {
 
 }
 
-void processLine(const String& line) {
-    if (line[0] == 'r') {
-        uint8_t addr = line[1] - '0';
-        uint8_t bytes = line[2] - '0';
-
-        Serial.print("Requesting ");
-        Serial.print(int(bytes));
-        Serial.print(" bytes from addr ");
-        Serial.println(int(addr));
-
-        Wire.requestFrom(addr, bytes);
-        Serial.print(" >>> ");
-        while (Wire.available()) {
-            char c = Wire.read();
-            Serial.print(c);
+void debug(const String& line, bool force=false) {
+    if (showDebug || force) {
+        Serial.print("[DBG] ");
+        for (int i = 0; i < line.length(); i++) {
+            Serial.print(line[i]);
+            if (line[i] == '\n')
+                Serial.print("[DBG] ");
         }
         Serial.println();
     }
+}
 
-    if (line[0] == 'w') {
-        uint8_t addr = line[1] - '0';
-        String txt = line.substring(2);
+void parseAddress(const char* line) {
+    // Parse hex byte from line and save to address variable
+}
 
-        Serial.print("Sending to addr ");
-        Serial.print(int(addr));
-        Serial.print(" the data '");
-        Serial.print(txt);
-        Serial.println("'");
+void readBytes(const char* line) {
+    // parse number of bytes from line. Read from I2C and print hex-representation of read bytes to Serial
+}
 
-        Wire.beginTransmission(addr);
-        Wire.write(txt.c_str());
-        Wire.endTransmission();
+void writeBytes(const char* line) {
+    // parse the bytes from the line and send them to the device via I2C.
+}
+
+void processLine(const String& line) {
+    if (line[0] == 'a') {
+        parseAddress(line+1);
+    }
+    else if (line[0] == 'r') {
+        readBytes(line+1);
+    }
+
+    else if (line[0] == 'w') {
+        writeBytes(line+1);
+    }
+    else {
+        debug("Help: (xx = hex byte) \n\n"
+              "a xx\\n : set target address\n"
+              "w xx xx xx xx xx ...\\n : write given byte sequence\n"
+              "r xx\\n : read xx bytes from device");
     }
 
 }
